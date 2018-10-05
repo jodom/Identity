ac447e7e9c6a77e0deec31bbd70264562c8295ca
Jodom <jodomouma@gmail.com>
2018-10-05 09:23:00 -0700

    medium rss

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,35 @@
-$( function () {
-    // helpers
+domReady( function () {
 
-    // get posts
+    const blog = document.getElementById('content');
+    console.log('blog', blog);
+
+    const dataUrl = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konuko_j'
+
+    fetch(dataUrl)
+        .then( (response) => {
+            if (response.status !== 200) {
+                console.log('Error fetching medium posts by Jodom');
+                return;
+            }
+            response.json()
+                .then( results => {
+                    const data = results.items.filter(post => {
+                        return post.categories.length > 0
+                    });
+                    console.log('posts', data);
+
+                    let posts = '<ul>'
+                    data.forEach(post => {
+                        let content = post.content.substr(0, 400);
+                        content = content.substr(0, Math.min(content.length, content.lastIndexOf(" ")));
+
+                        posts += `<li><a target="_blank" href="${post.link}"><h3>${post.title}</h3><a><p>${content}</p></li>`;
+                    })
+                    posts += '</ul>';
+
+                    blog.innerHTML = posts;
+                });
+        });
 
     // const currentYear = Date.today();
 
