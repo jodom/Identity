8e68151fb39fb56c386c5c6493a41abdc7f6f8fb
Konuko Konuko <jokonuko@microsoft.com>
2019-03-09 19:00:36 -0800

    profile componnet

--- src/components/Identity.tsx
+++ src/components/Identity.tsx
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import './Identity.css';
+
+import { Profile, ProfileProps } from './Profile';
+
+const profileProps: ProfileProps = {
+  name: '',
+  image: {
+    url: 'https://res.cloudinary.com/dzx2hnetf/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1552174621/identity/profile.jpg',
+    alt: 'profile picture'
+  },
+  tagline: 'engineer | art | software'
+};
 
 export const Identity  = React.memo(() => {
   return (
     <div className="Identity">
-      <div className="profile">
-        <div className="avatar">
-          <img src="images/profile.jpeg" alt="" />
-        </div>
-        <div>
-          <h1>Konuko Jodom</h1>
-          <p>ENGINEER ART SOFTWARE</p>
-        </div>
-      </div>
+      <Profile {...profileProps}/>
       <div className="social-well">
         <a target="blank_" href="https://github.com/jodom" className="fa fa-github">Github</a>
         <a target="blank_" href="https://linkedin.com/in/konukojodom" className="fa fa-linkedin">LinkedIn</a>
---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+export interface ProfileProps {
+  name: string,
+  image: {
+    url: string,
+    alt: string
+  }
+  tagline: string
+}
+
+export const Profile = React.memo((props: ProfileProps) => {
+  const {name, image, tagline} = props;
+  return (
+    <div className="profile">
+      <div className="avatar">
+        <img src={image.url} alt={image.alt} />
+      </div>
+      <div>
+        <span className="name">{name}</span>
+        <span className="tagline">{tagline}</span>
+      </div>
+    </div>
+  );
+});
