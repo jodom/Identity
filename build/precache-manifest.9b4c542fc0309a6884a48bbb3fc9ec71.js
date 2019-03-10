58912f6ce0653e65f796b32c606d734f2151a827
Konuko Konuko <jokonuko@microsoft.com>
2019-03-09 22:24:33 -0800

    build

---
+++ build/precache-manifest.9b4c542fc0309a6884a48bbb3fc9ec71.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/identity/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "5abfe59af78a1e3524c4",
+    "url": "/identity/static/css/main.ec79e355.chunk.css"
+  },
+  {
+    "revision": "00a8acf75b2fb84e5d25",
+    "url": "/identity/static/js/runtime~main.192f1642.js"
+  },
+  {
+    "revision": "9989153f7edce7acf892",
+    "url": "/identity/static/js/2.2e184cb3.chunk.js"
+  },
+  {
+    "revision": "efa83c958b082bb4e3b6b5b33c9957ed",
+    "url": "/identity/static/media/bg.efa83c95.jpg"
+  },
+  {
+    "revision": "5abfe59af78a1e3524c4",
+    "url": "/identity/static/js/main.8afa0370.chunk.js"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/identity/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/identity/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/identity/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "24c601e721ebd8279d38e2cfa0d01bc6",
+    "url": "/identity/static/media/fontawesome-webfont.24c601e7.svg"
+  },
+  {
+    "revision": "29967bcd686a7940a6e97114b80a89a9",
+    "url": "/identity/index.html"
+  }
+];
