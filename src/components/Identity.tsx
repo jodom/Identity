6e59cb9c5c65996cbadb9315bc0ca142f08af6d0
Konuko Konuko <jokonuko@microsoft.com>
2019-03-09 17:25:23 -0800

    take me to the body shop

--- src/components/Identity.tsx
+++ src/components/Identity.tsx
@@ -4,26 +4,24 @@ import './Identity.css';
 class Identity extends Component {
   render() {
     return (
-      <div className="App">
-        <section id="main">
-            <div className="profile">
-                <div className="avatar">
-                  <img src="images/profile.jpeg" alt="" />
-                </div>
-                <div>
-                  <h1>Konuko Jodom</h1>
-                  <p>Futurist | Developer | Engineer</p>
-                </div>
-            </div>
-            <div className="social-well">
-              <a target="blank_" href="https://twitter.com/@konuko_j" className="fa-twitter">Twitter</a>
-              <a target="blank_" href="https://linkedin.com/in/konukojodom" className="fa-linkedin">LinkedIn</a>
-              <a target="blank_" href="https://github.com/jodom" className="fa-github">Github</a>
-            </div>
-            <div className="blog-cta">
-                <a target="blank_" href="https://github.com/jodom">READ MY MUSINGS <i className="fa fa-medium"></i></a>
-            </div>
-        </section>
+      <div id="main">
+        <div className="profile">
+          <div className="avatar">
+            <img src="images/profile.jpeg" alt="" />
+          </div>
+          <div>
+            <h1>Konuko Jodom</h1>
+            <p>Futurist | Developer | Engineer</p>
+          </div>
+        </div>
+        <div className="social-well">
+          <a target="blank_" href="https://twitter.com/@konuko_j" className="fa-twitter">Twitter</a>
+          <a target="blank_" href="https://linkedin.com/in/konukojodom" className="fa-linkedin">LinkedIn</a>
+          <a target="blank_" href="https://github.com/jodom" className="fa-github">Github</a>
+        </div>
+        <div className="blog-cta">
+          <a target="blank_" href="https://github.com/jodom">READ MY MUSINGS <i className="fa fa-medium"></i></a>
+        </div>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import Identity from './components/Identity';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Identity />, document.getElementById('root'));
+ReactDOM.render(<Identity />, document.getElementById('wrapper'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
